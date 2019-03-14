0e2d0f9766963e296c487e5927839b3e24ec74d7
crowisover2 <35515041+crowisover2@users.noreply.github.com>
2019-03-14 10:31:36 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace ConsoleApp2
+{
+
+
+    /*
+     Belum Selesai karena Masih mencari bagaimana cara encapsulasi data pada C#
+     */
+
+    public class byteData
+    {
+        public byte[] Data;
+
+        public static class SeDeData
+        {
+            public static M serializable(object O)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    (new BinaryFormatter()).Serialize(memoryStream, O);
+                    SerializableAttribute serializableAttribute = new SerializableAttribute
+                    return new SerializableAttribute(Data = memoryStream.ToArray());
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    
+}
---
+++ Readme.txt
@@ -0,0 +1,12 @@
+Analisa Game CS-GO
+
+Data yang perlu dienkapsulasi adalah :
+
+- ID player
+- ID ModelCharacter
+- Koordinat player
+- Koordinat bullet
+- ID Senjata yang digunakan
+- Status dead/alive
+
+Karena dalam game, keadaan tersebut yang selalu berubah ubah.
